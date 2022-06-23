f88ec98df5a48fb13611a62e394daf6f21d1f5df
Tomohiko Mukai <contact@mukai-lab.org>
2022-06-23 14:18:35 +0900

    コメント修正

--- Assets/BoidMotion.cs
+++ Assets/BoidMotion.cs
@@ -55,36 +55,26 @@ public class BoidMotion : MonoBehaviour
 
         // 新しい移動方向（色々な力によって修正されていく）
         Vector3 newDirection = _direction;//とりあえず、行きたい方向を代入しておく
-
         //  1. リーダーを探す（inspectorで設定済み）
         //  2. リーダーの位置と、自分の位置を調べる
         LeaderMotion leaderMotion = _leader.GetComponent<LeaderMotion>();
-        // リーダーがいる方向に向かう力
+        // 3. リーダーがいる方向に向かう力を算出
         Vector3 leaderForce = leaderMotion.Position - this._position;
-        // 自分が行きたい方向と、リーダーへ向かう力の足し算＝修正された移動方向
+        // 自分が行きたい方向と、リーダーへ向かう力との足し算＝修正された移動方向
         newDirection = newDirection + leaderForce;
-
-        // 他のボイドの位置を知り、一定距離内にいるものから離れるための力を計算して、移動方向newDirectionを修正
-        for (int i = 0; i < _boids.Length; ++i)
-        {
-            if (_boids[i].name != this.gameObject.name)// 他のボイド？
-            {
+        // 他のボイドの位置を求め、一定距離内にいるものから離れるための力を計算することで、移動方向newDirectionを修正
+        for (int i = 0; i < _boids.Length; ++i) {
+            if (_boids[i].name != this.gameObject.name) {// 他のボイド？
                 BoidMotion boidMotion = _boids[i].GetComponent<BoidMotion>(); //BoidMotionコンポーネント
-                //  一定距離の内外をチェック
-                //   距離の計算　＝　// TODO: this.position, boidMotion.Position の間の距離を計算
+                //  他ボイドとの距離を計算
                 float distance = Vector3.Distance(this._position, boidMotion.Position);
-                //   求まった距離が、ある値未満か、ある値以上かをチェック
-                if (distance < 2.0f)
-                {
+                //  求まった距離が、ある値未満であれば（近すぎるのであれば）避ける力を計算して移動方向を修正
+                if (distance < 2.0f) {
                     Debug.Log("NEAR!");
                     newDirection = newDirection + ???; // 避ける力を計算して移動方向を修正
                 }
-
-                // boids[i].transform.position (あえて使わない)
-                //Debug.Log(boids[i].name + ", " + this.gameObject.name);
             }
         }
-
         // いろいろ修正された移動方向を正規化
         _direction = newDirection.normalized; // 長さ1にする
         // 次の位置 = 現在位置と、速度 * 移動方向 * Time.deltaTimeの和
